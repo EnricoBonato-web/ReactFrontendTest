da9e75dd49f2ebadb8076b8f4bb2c08eaa5bc89e
Enrico <enrico.bonato95@gmail.com>
2022-06-27 18:44:26 +0200

    cose

--- src/components/Submit/AnswerItem.js
+++ src/components/Submit/AnswerItem.js
@@ -1,11 +1,12 @@
+import classes from './AnswerItem.module.css'
 const AnswerItem = (props) => {
   const { id, value } = props;
 
   return (
-    <li key={id}>
-      <h3>
+    <li key={id} className={classes.answer}>
+      <p>
         {id} - {value}
-      </h3>
+      </p>
     </li>
   );
 };
